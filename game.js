a71be9b17204e14390f074b081f2b3d323995921
herohamp <hamptonmoore@herohamp.local>
2016-07-09 17:36:39 -0400

    heyyyyy

--- game.js
+++ game.js
@@ -173,8 +173,8 @@ console.log(rect2);
 }
 }
 function intersectRect(rect1, rect2) {
-	if (rect1.y < rect2.y + rect2.h &&
-   rect1.y + rect1.w > rect2.y &&
+	if (-(rect1.y) < -(rect2.y) + rect2.h &&
+   -(rect1.y) + rect1.w > -(rect2.y) &&
    rect1.x < rect2.x + rect2.w &&
    rect1.h + rect1.x > rect2.x) {
 		var socket = SOCKET_LIST[findID(rect1.name)];
